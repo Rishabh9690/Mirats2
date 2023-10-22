bc2c41436d4a2871438325eee3fccdd10f4da8e1
Rishabh Saxena <rishabh9690@gmail.com>
2023-10-22 23:06:48 +0530

    updated 2.)

--- src/components/companies/globalOffices/offices/MiratsLucknow.js
+++ src/components/companies/globalOffices/offices/MiratsLucknow.js
@@ -9,9 +9,9 @@ import MiratsLkw from "../../../images/images/MiratsLucknow.png";
 import UsOffice from "../../../images/images/UsOffice.png";
 import Atomos from "../../../images/images/Atomos.png";
 
-const MiratsLucknow=()=>{
+const MiratsLucknow = () => {
 
-    const officeDetails=[{
+    const officeDetails = [{
         name: "Macer India Research and Technology Services Private Limited",
         address: "1st Floor, Pitambara House, Tedhi Pulia Ring Rd, Kalyanpur, Lucknow, Uttar Pradesh",
         pin: "Pin Code : 226022",
@@ -23,22 +23,22 @@ const MiratsLucknow=()=>{
         rocCode: "ROC Code : Roc-Kanpur"
     }]
 
-    const openingHours=[{
+    const openingHours = [{
         day: "Mon",
         time: "10:00 a.m. - 8:00 p.m"
-    },{
+    }, {
         day: "Tue",
         time: "10:00 a.m. - 8:00 p.m"
-    },{
+    }, {
         day: "Wed",
         time: "10:00 a.m. - 8:00 p.m"
-    },{
+    }, {
         day: "Thu",
         time: "10:00 a.m. - 8:00 p.m"
-    },{
+    }, {
         day: "Fri",
         time: "10:00 a.m. - 8:00 p.m"
-    },{
+    }, {
         day: "Sat",
         time: "10:00 a.m. - 8:00 p.m"
     }, {
@@ -46,7 +46,7 @@ const MiratsLucknow=()=>{
         time: "Closed"
     }]
 
-    const headQuarters=[{
+    const headQuarters = [{
         image: UsOffice,
         name: "",
         address: "Mirats Insights LLC, Wyoming, USA",
@@ -54,125 +54,124 @@ const MiratsLucknow=()=>{
         contactNo: "Contact No. +1 782 457"
     }]
 
-    const handleHeadQuarters=(e)=>{
+    const handleHeadQuarters = (e) => {
         e.preventDefault();
-        let index=Number(e.currentTarget.id);
-        if(index===0)
-        {
+        let index = Number(e.currentTarget.id);
+        if (index === 0) {
             console.log("Clicked");
             window.location.replace("/company/globaloffices/UsMiratsoffice");
         }
     }
     return (
         <div className="officed_body">
-        {/* <img src={banner} alt="banner" /> */}
-        <div className="offices_level_one">
-                <img style={{width: "90%"}} src={banner} alt="banner" />
+            {/* <img src={banner} alt="banner" /> */}
+            <div className="offices_level_one">
+                <img style={{ width: "90%" }} src={banner} alt="banner" />
                 <section>
                     <p>Global Offices</p>
                     <h2>Mirats Insights Private Limited,<br />Lucknow, India</h2>
-                </section>                
-            </div>
-        <div className="offices_level_two">
-            <section>
-                <h2>Office Details</h2>
-                <table>
-                    <thead>
-                        <tr>
-                            <th className="offices_level_two_head">Mirats Insights Private Limited,</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {officeDetails.map((element, index)=>{
-                            return(
-                                <tr className="offices_level_two_office_details" key={index}>
-                                    <tr>{element.address}</tr>
-                                    <tr>{element.pin}</tr>
-                                    <tr>{element.cin}</tr>
-                                    <tr>{element.incorporationDate}</tr>
-                                    <tr>{element.agmDate}</tr>
-                                    <tr>{element.balanceSheetDate}</tr>
-                                    <tr>{element.listingStatus}</tr>
-                                    <tr>{element.rocCode}</tr>
-                                </tr> 
-                            )
-                        })}
-                    </tbody>
-                </table>
-            </section>
-            <section>
-                <h2>Office Opening Hours</h2>
-                <table>
-                    <thead>
-                        <tr>
-                            <th className="offices_level_two_head_timmings">Days</th>
-                            <th className="offices_level_two_head_timmings">Time</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {openingHours.map((element, index)=>{
-                            return(
-                                <tr className="offices_level_two_office_timmings" key={index}>
-                                    <td>{element.day}</td>
-                                    <td>{element.time}</td>
-                                </tr>
-                            )
-                        })}
-                    </tbody>
-                </table>
-            </section>
-        </div>
-        <hr></hr>
-        <div className="offices_level_three">
-            <h2>Contact</h2>
-            <div className="offices_level_three_sec">
-                <section>
-                    <img src={call} alt="call" />
-                    <div className="offices_level_three_sec_inline">
-                        <p>Contact No.</p>
-                        <h5>+91 78245 78245</h5>
-                    </div>
-                </section>
-                <section>
-                    <img src={mail} alt="mail" />
-                    <div className="offices_level_three_sec_inline">
-                        <p>Email Id</p>
-                        <h5>miratsinsights@gmail.com</h5>
-                    </div>
                 </section>
             </div>
-            <div className="offices_level_three_address">
-                <img src={location} alt="location" />
+            <div className="offices_level_two">
                 <section>
-                    <p>Address</p>
-                    <h5>MMS-2/12A, Ground Floor, Sector - C, SRY, Aliganj, Lucknow, Uttar Pradesh - 226021</h5>
+                    <h2>Office Details</h2>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th className="offices_level_two_head">Mirats Insights Private Limited,</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {officeDetails.map((element, index) => {
+                                return (
+                                    <tr className="offices_level_two_office_details" key={index}>
+                                        <tr>{element.address}</tr>
+                                        <tr>{element.pin}</tr>
+                                        <tr>{element.cin}</tr>
+                                        <tr>{element.incorporationDate}</tr>
+                                        <tr>{element.agmDate}</tr>
+                                        <tr>{element.balanceSheetDate}</tr>
+                                        <tr>{element.listingStatus}</tr>
+                                        <tr>{element.rocCode}</tr>
+                                    </tr>
+                                )
+                            })}
+                        </tbody>
+                    </table>
+                </section>
+                <section>
+                    <h2>Office Opening Hours</h2>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th className="offices_level_two_head_timmings">Days</th>
+                                <th className="offices_level_two_head_timmings">Time</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {openingHours.map((element, index) => {
+                                return (
+                                    <tr className="offices_level_two_office_timmings" key={index}>
+                                        <td>{element.day}</td>
+                                        <td>{element.time}</td>
+                                    </tr>
+                                )
+                            })}
+                        </tbody>
+                    </table>
                 </section>
             </div>
-        </div>
-        <hr></hr>
-        <div className="offices_level_four">
-            <h2>Virtual Office</h2>
-            <div className="offices_level_four_sec">
-                <img className="slideing_image" src={left} alt="left" />
-                <div className="offices_level_four_sec_parent_one_office">
-                {headQuarters.map((element, index)=>{
-                    return(
-                        <div className="offices_level_four_sec_child" id={index} onClick={handleHeadQuarters}>
-                                <img src={element.image} alt="image"/>
-                                <section>
-                                    <h2>{element.name}</h2>
-                                    <h2>{element.address}</h2>
-                                    <p>{element.email}</p>
-                                    <p>{element.contactNo}</p>
-                                </section>
-                            </div>
-                    )
-                })}
+            <hr></hr>
+            <div className="offices_level_three">
+                <h2>Contact</h2>
+                <div className="offices_level_three_sec">
+                    <section>
+                        <img src={call} alt="call" />
+                        <div className="offices_level_three_sec_inline">
+                            <p>Contact No.</p>
+                            <h5>+91 78245 78245</h5>
+                        </div>
+                    </section>
+                    <section>
+                        <img src={mail} alt="mail" />
+                        <div className="offices_level_three_sec_inline">
+                            <p>Email Id</p>
+                            <h5>miratsinsights@gmail.com</h5>
+                        </div>
+                    </section>
                 </div>
-                <img className="slideing_image" src={right} alt="right" />
+                <div className="offices_level_three_address">
+                    <img src={location} alt="location" />
+                    <section>
+                        <p>Address</p>
+                        <h5>MMS-2/12A, Ground Floor, Sector - C, SRY, Aliganj, Lucknow, Uttar Pradesh - 226021</h5>
+                    </section>
+                </div>
+            </div>
+            <hr></hr>
+            <div className="offices_level_four">
+                <h2>Virtual Office</h2>
+                <div className="offices_level_four_sec">
+                    <img className="slideing_image" src={left} alt="left" />
+                    <div className="offices_level_four_sec_parent_one_office">
+                        {headQuarters.map((element, index) => {
+                            return (
+                                <div className="offices_level_four_sec_child" id={index} onClick={handleHeadQuarters}>
+                                    <img src={element.image} alt="image" />
+                                    <section>
+                                        <h2>{element.name}</h2>
+                                        <h2>{element.address}</h2>
+                                        <p>{element.email}</p>
+                                        <p>{element.contactNo}</p>
+                                    </section>
+                                </div>
+                            )
+                        })}
+                    </div>
+                    <img className="slideing_image" src={right} alt="right" />
+                </div>
             </div>
         </div>
-    </div>
     )
 }
 
